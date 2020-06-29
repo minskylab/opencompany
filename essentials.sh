f45b2052a21b40b3d865f68ba1599e3d17c59795
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-06-29 15:55:36 -0500

    first commit

---
+++ essentials.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.3/aio/deploy/recommended.yaml
+curl -fsSL https://github.com/kubedb/installer/raw/v0.13.0-rc.0/deploy/kubedb.sh | bash
