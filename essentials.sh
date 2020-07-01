f03c242d59664e4596afe25b98a9115f6baf2f26
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-07-01 17:15:15 -0500

    backup

--- essentials.sh
+++ essentials.sh
@@ -1,3 +1,2 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.3/aio/deploy/recommended.yaml
-curl -fsSL https://github.com/kubedb/installer/raw/v0.13.0-rc.0/deploy/kubedb.sh | bash
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.1/cert-manager.yaml
