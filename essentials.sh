3bb055f0ea619b33c513b3527bc2c68b63bc05b4
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-07-03 17:59:52 -0500

    more resources

--- essentials.sh
+++ essentials.sh
@@ -1,2 +1,3 @@
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.3/aio/deploy/recommended.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.3/aio/deploy/recommended.yaml
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.1/cert-manager.yaml
+# kubectl create -f https://raw.githubusercontent.com/hasura/gitkube/master/manifests/gitkube-setup.yaml
